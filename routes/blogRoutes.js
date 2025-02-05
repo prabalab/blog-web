4a06fe848607c8c5e31e8ef9a7b1fb4542a83909
prabalab <40490211+prabalab@users.noreply.github.com>
2025-02-05 14:12:39 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -23,4 +23,14 @@ router.post("/submit", authMiddleware, async (req, res) => {
     }
 });
 
+// Fetch all blogs of the logged-in user
+router.get("/user-blogs", authMiddleware, async (req, res) => {
+    try {
+        const blogs = await Blog.find({ userId: req.user.userId }).sort({ createdAt: -1 });
+        res.json(blogs);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+});
+
 module.exports = router;
