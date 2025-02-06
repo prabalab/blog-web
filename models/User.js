1ba8969911b253c9249665f5f568a41a2b051341
prabalab <40490211+prabalab@users.noreply.github.com>
2025-02-06 19:15:47 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isVerified: { type: Boolean, default: false }, // Email verification status
-    otp: { type: String }, // OTP for email verification
-    otpExpires: { type: Date }, // OTP expiration time
-  },
-  { timestamps: true } // Adds createdAt and updatedAt automatically
-);
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
 module.exports = mongoose.model("User", UserSchema);
