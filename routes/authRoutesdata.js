91ff832b5a2118f46ba2b395732d821ee4078d92
prabalab <40490211+prabalab@users.noreply.github.com>
2025-02-07 19:21:12 +0530

    Update authRoutesdata.js

--- routes/authRoutesdata.js
+++ routes/authRoutesdata.js
@@ -149,4 +149,29 @@ router.delete('/usersdata/:id', async (req, res) => {
   }
 });
 
+//login router 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    // Check if user is verified
+    if (!user.isVerified) return res.status(400).json({ message: "User not verified" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT Token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "5m" });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
+
 module.exports = router;
